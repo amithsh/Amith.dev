cb3e6c8b4482f62f89788be1b9b6b8168de40693
amithsh <amithsh726@gmail.com>
2023-12-16 00:49:59 +0530

    added the media element for the small screen

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,13 @@
 
 import Animate from "@/components/Animate";
+import Workpage from "./works/page";
 
 
 export default function Home() {
   return (
     <div>
      <Animate />
+     <Workpage />
     </div>
   );
 }
--- app/(root)/(routes)/works/page.tsx
+++ app/(root)/(routes)/works/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const Workpage = () => {
   return (
-    <div>page</div>
+    <div className='h-screen w-full flex items-center justify-center sm:h-screen sm:w-full md:h-screen md:w-full bg-slate-400'>
+      this is work page
+    </div>
   )
 }
 
-export default page
+export default Workpage
